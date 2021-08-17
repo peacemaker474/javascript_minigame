00ccf49c78b50027192409b3a95ec6f5522bd839
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-08-18 01:24:51 +0900

    2021.08.17. 사칙연산 부분.

--- calculator/src/js/index.js
+++ calculator/src/js/index.js
@@ -1,7 +1,29 @@
 const digitNumber = document.querySelectorAll(".digit");
 const totalNumber = document.getElementById("total");
+const operation = document.querySelectorAll(".operation");
 
 let addNumber = "";
+
+const handlecalculator = (evt) => {
+    const fnOperation = evt.target.textContent;
+    switch (fnOperation) {
+        case "/":
+            console.log("나누기");
+            break;
+        case "X":
+            console.log("곱하기");
+            break;
+        case "-":
+            console.log("빼기");
+            break;
+        case "+":
+            console.log("더하기");
+            break;
+        case "=":
+            console.log("결과");
+            break;
+    }
+}
 
 const handleInputNumber = (evt) => {
     addNumber += evt.target.textContent;
@@ -10,6 +32,7 @@ const handleInputNumber = (evt) => {
 
 function init() {
     Array.from(digitNumber).forEach(number => number.addEventListener("click", handleInputNumber));
+    Array.from(operation).forEach(item => item.addEventListener("click", handlecalculator));
 }
 
 init();
