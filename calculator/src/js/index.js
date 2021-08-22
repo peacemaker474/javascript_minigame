0486f2530b231112a2a8f7a4c3915370d6981bb8
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-08-22 18:26:59 +0900

    2021.08.22. 완료(오류는 추후 수정)

--- calculator/src/js/index.js
+++ calculator/src/js/index.js
@@ -3,12 +3,14 @@ const showNumber = document.getElementById("total");
 const operation = document.querySelectorAll(".operation");
 const resetBtn = document.querySelector(".modifiers");
 
+// 1,2 번째 숫자, 계산 결과, 사칙연산 전역변수 처리
 let firstNumber = "";
 let secondNumber = "";
 let calculate = "";
 let totalNumber = 0;
 let result = "";
 
+// 정수와 실수 구분 영역
 const checkTypeNumber = () => {
     let checkFirst = 0;
     let checkSecond = 0;
@@ -29,6 +31,7 @@ const checkTypeNumber = () => {
     }
 }
 
+// 계산하는 영역
 const handlecalculater = () => {
     checkTypeNumber();
     switch (calculate) {
@@ -51,14 +54,21 @@ const handlecalculater = () => {
     calculate = "";
     result = "";
 }
+// 사칙 연산 중복 체크
+const checkDoubleOperation = () => {
+    if (secondNumber !== "") {
+        handlecalculater()
+    }
+}
 
+// 사칙연산 영역
 const handleDivideOperation = (evt) => {
     if (evt.target.textContent === "=") {
         result = evt.target.textContent;
     }
 
     if (calculate !== "") {
-        if (secondNumber !== "") handlecalculater()
+        checkDoubleOperation();
         calculate = evt.target.textContent;
     } else {
         calculate = evt.target.textContent;
@@ -67,6 +77,53 @@ const handleDivideOperation = (evt) => {
     if (result === "=") handlecalculater()
 }
 
+// 숫자 영역에서 "." 중복,  초기 0 입력 부분 유효성 검사
+const checkDoubleDot = (currentNumber) => {
+    if (firstNumber === "0" && currentNumber === ".") {
+        firstNumber += currentNumber;
+        showNumber.textContent = firstNumber;
+    } else if (firstNumber === "0" && currentNumber !== ".") {
+        firstNumber = "0";
+        showNumber.textContent = firstNumber;
+    } else if (firstNumber.indexOf(".") !== -1 && currentNumber === ".") {
+        return false;
+    }
+    else {
+        firstNumber += currentNumber;
+        showNumber.textContent = firstNumber;
+    }
+}
+
+const checkSecondNumber = (currentNumber) => {
+    if (secondNumber.indexOf(".") !== -1 && currentNumber === ".") {
+        return false;
+    } else {
+        secondNumber += currentNumber;
+        showNumber.textContent = secondNumber;
+    }
+}
+
+const checkFirstNumber = (currentNumber) => {
+    if (firstNumber === "") {
+        firstNumber += currentNumber;
+        showNumber.textContent = firstNumber;
+    } else {
+        checkDoubleDot(currentNumber);
+    }
+}
+
+// 숫자 버튼 클릭 영역
+const handleInputNumber = (evt) => {
+    let currentNumber = evt.target.textContent;
+
+    if (calculate === "") {
+        checkFirstNumber(currentNumber);
+    } else {
+        checkSecondNumber(currentNumber);
+    }
+}
+
+// AC 버튼 영역
 const handleResetNumber = () => {
     firstNumber = "";
     secondNumber = "";
@@ -75,30 +132,6 @@ const handleResetNumber = () => {
     showNumber.textContent = 0;
 }
 
-const handleInputNumber = (evt) => {
-    let currentNumber = evt.target.textContent;
-
-    if (calculate === "") {
-        if (firstNumber === "") {
-            firstNumber += currentNumber;
-            showNumber.textContent = firstNumber;
-        } else {
-            if (firstNumber === "0" && currentNumber === ".") {
-                firstNumber += currentNumber;
-                showNumber.textContent = firstNumber;
-            } else if (firstNumber === "0" && currentNumber !== ".") {
-                firstNumber = "0";
-                showNumber.textContent = firstNumber;
-            } else {
-                firstNumber += currentNumber;
-                showNumber.textContent = firstNumber;
-            }
-        }
-    } else {
-        secondNumber += currentNumber;
-        showNumber.textContent = secondNumber;
-    }
-}
 
 function init() {
     Array.from(digitNumber).forEach(number => number.addEventListener("click", handleInputNumber));
